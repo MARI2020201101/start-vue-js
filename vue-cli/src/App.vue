e47a85baa3c99f63f1c9c31f08b229d9bd6c79c2
MARI <mari20201101@gmail.com>
2021-08-29 14:25:56 +0900

    Use props & data properties

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,21 @@
 <template>
+  
   <div>
     {{str}}
+     <app-header v-bind:propsData="str"></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
   export default{
     data:function(){
       return {
         str:'hi'
       }
+    },
+    components:{
+      'app-header':AppHeader
     }
   }
 </script>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>header</h1>
+      {{ propsData }}
+  </div>
+</template>
+
+<script>
+  export default{
+    props:['propsData']
+  }
+</script>
+
+<style>
+
+</style>
